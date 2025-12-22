cea93ad8365374e90e9f129fa95f8d6f917af9b8
A-French-PngHash <75045140+A-French-PngHash@users.noreply.github.com>
2025-12-22 14:57:02 +0100

    minor change

--- titouan/particles.py
+++ titouan/particles.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 class Particles:
-    positions = []
-    velocities = []
+    positions = np.array([])
+    velocities = np.array([])
     colided : np.array
 
     def __init__(self, n, s, c1, c2, w):
         self.n, self.s, self.c1, self.c2, self.w = n, s, c1, c2, w
-        self.positions.append(np.zeros((n, 2)))
-        self.velocities.append(np.zeros((n,2)))
+        self.positions = np.append(self.positions,np.zeros((n, 2)))
+        self.velocities = np.append(self.velocities,np.zeros((n, 2)))
         self.colided = np.zeros(n).astype(bool)
-        
+
+particles = Particles(100, 1, 2, 3, 4)
+print(particles.positions.shape)
+print(particles.positions)
+    
+    
